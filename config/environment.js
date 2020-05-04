bc380e935e54d1e2f8e81b3a0866724d33195def
jeffrey gaboni <mrgabonijeff@gmail.com>
2020-05-04 10:47:41 +0800

    Update environment.js

--- config/environment.js
+++ config/environment.js
@@ -24,15 +24,15 @@ module.exports = function(environment) {
     },
 
     blog: {
-      title: 'empress-blog',
-      description: 'Static Blog System - Built with Ember',
+      title: 'Balitang PH',
+      description: 'Balitang PH',
       coverImage: '/images/blog-cover.jpg',
 
       navigation: [{
         label: 'Home',
         route: 'index'
       }, {
-        label: 'Built by Chris Manson',
+        label: 'Built by Balitang PH',
         route: 'page',
         id: 'chris-manson'
       }]
